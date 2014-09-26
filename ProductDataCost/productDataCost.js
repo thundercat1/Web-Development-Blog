41a1ddc131f3a9d93e1a1fdd77687eb37dfb4bff
Thundercat <michael.c.haines@gmail.com>
2014-09-25 20:40:33 -0600

    trying to add some better audit logic

--- ProductDataCost/productDataCost.js
+++ ProductDataCost/productDataCost.js
@@ -8,7 +8,7 @@ angularApp.controller('productDataAppController', function($scope, peopleData, t
 	//set initial values for input form
 	var reset = function() {
 		$scope.chosenPerson = {"name": "Nobody", "rate": 0};
-		$scope.chosenTaxonomy = {"name": "Nothing", "annualCreation": 0, "lifeSpanYears": 0};
+		$scope.chosenTaxonomy = {"name": "Nothing", "annualCreation": 0, "lifeSpanMonths": 0};
 		$scope.dataInputTimeToCreate = null;
 		$scope.dataInputMonthsToRecheck = null;
 		$scope.newFieldName = null;
@@ -32,23 +32,51 @@ angularApp.controller('productDataAppController', function($scope, peopleData, t
 	};
 
 	$scope.addNewDataField = function(field) {
-		if ($scope.chosenTaxonomy.name != "Nothing" && $scope.chosenPerson.name != "Nobody" && (typeof($scope.dataInputMonthsToRecheck)=="number") && (typeof($scope.dataInputTimeToCreate)=="number")) {
+		if ($scope.chosenTaxonomy.name != "Nothing" && $scope.chosenPerson.name != "Nobody" && (typeof($scope.dataInputMonthsToRecheck)=="number")
+		    && (typeof($scope.dataInputTimeToCreate)=="number")) {
+			    var l = $scope.dataInputMonthsToRecheck / 12.0;
+			    var R = $scope.chosenTaxonomy.annualCreation;
+			    var t0 = $scope.chosenTaxonomy.lifeSpanMonths / 12.0;
 
-			var creationCount = $scope.chosenTaxonomy.annualCreation;
-			var maintenanceCount = creationCount * $scope.chosenTaxonomy.lifeSpanYears * (12 / $scope.dataInputMonthsToRecheck);
+			    var auditPeriods = [];
+			    for (i=0; i < 50; i++) {
+				    auditPeriods.push(i*l);
+			    }
 
-			var annualCost = ((creationCount + maintenanceCount) * ($scope.dataInputTimeToCreate / 3600) * $scope.chosenPerson.rate);
+			    var auditCount = 0;
+
+
+
+		    }
+
+            var t0 = $scope.chosenTaxonomy.lifeSpanMonths;
+
+            //N is monthly creation
+            var N = $scope.chosenTaxonomy.annualCreation/12;
+
+            //Data audit period
+            var l = $scope.dataInputMonthsToRecheck;
+
+
+
+            var touches = 0;
+            for (i = 0; i <= r; i++) {
+                touches = touches + ((N * (i/r)));
+            }
+
+            var annualCost = ($scope.dataInputTimeToCreate / 3600) * $scope.chosenPerson.rate * touches;
 
 			$scope.newDataFields.push(
 					{	"name": $scope.newFieldName,
 						"responsible": $scope.chosenPerson,
-				"annualCost": annualCost
+				        "annualCost": annualCost
 					}
 					);
 
 			reset();
 		}
 	};
+
 
 	$scope.remove = function(field) {
 		var index = $scope.newDataFields.indexOf(field);
@@ -63,15 +91,17 @@ angularApp.factory('peopleData', function() {
 		{"name": "Buyer", "rate": 23},
 		{"name": "Product Data", "rate": 15},
 		{"name": "Visual Merch", "rate": 14},
-		{"name": "Marketing", "rate": 20}
+		{"name": "Marketing", "rate": 20},
+		{"name": "Unit Man", "rate": 1}
 	];
 });
 
 
 angularApp.factory('taxonomyData', function() {
 	return [
-		{"name": "sku", "annualCreation": 500000, "lifeSpanYears": 3},
-		{"name": "style", "annualCreation": 100000, "lifeSpanYears": 5},
-		{"name": "brand", "annualCreation": 500, "lifeSpanYears": 7}
+		{"name": "sku", "annualCreation": 530000, "lifeSpanMonths": 6},
+		{"name": "style", "annualCreation": 100000, "lifeSpanMonths": 6},
+		{"name": "brand", "annualCreation": 500, "lifeSpanMonths": 48},
+		{"name": "100 items with 6 month lifespan", "annualCreation": 100, "lifeSpanMonths": 6}
 	];
 });
